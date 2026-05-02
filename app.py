ac4592d157f456e90d61fcd4e065ff656163ae29
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-02 23:31:38 +0300

    Update header styles in app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if st.session_state.page == "home":
             <h1 style="margin:0; font-size: 35px;">التحضير التقني</h1>
             <h2 style="margin:0; font-size: 28px;">مدرسة القطيف الثانوية</h2>
             <p style="color:#ffd700; font-size:22px; margin-top:10px; font-weight: bold;">مدير المدرسة : أ. فراس آل عبدالمحسن</p>
-            <div style="font-size: 20px; margin-top: 10px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 10px;">
+            <div style="font-size: 22px; margin-top: 22px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 22px;">
              <h3 style="margin:0; font-size: 22px;">فكرة و برمجة </h3>
              أ.عارف أحمد الحداد
              <h4 style="margin:0; font-size: 22px;">2026</h4>
