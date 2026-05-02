bf608072119aadce49cf38cb52540eed26636d3f
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-02 23:34:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -68,9 +68,9 @@ if st.session_state.page == "home":
             <h2 style="margin:0; font-size: 28px;">مدرسة القطيف الثانوية</h2>
             <p style="color:#ffd700; font-size:22px; margin-top:10px; font-weight: bold;">مدير المدرسة : أ. فراس آل عبدالمحسن</p>
             <div style="font-size: 22px; margin-top: 22px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 22px;">
-             <h3 style="margin:0; font-size: 22px;">فكرة و برمجة </h3>
-             أ.عارف أحمد الحداد
-             <h4 style="margin:0; font-size: 22px;">2026</h4>
+            <h3 style="margin:0; font-size: 22px;">فكرة و برمجة </h3>
+            <h4 style="margin:0; font-size: 22px;">أ. عارف أحمد الحداد </h4>
+            <h5 style="margin:0; font-size: 22px;">2026</h5>
             </div>
         </div>
     ''', unsafe_allow_html=True)
