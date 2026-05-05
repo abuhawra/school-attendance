df92c5310d244cbd8b161a88f53843e3eeb0d2d7
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-05 07:54:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def get_wa_link(df, status_type, d):
 if st.session_state.page == "home":
     st.markdown('''
         <div class="main-header">
-            <h2 style="color:#ffd700; font-size: 55px; font-weight: 800;">بصمة تميز</h2>
-            <h2 style="color:#ffffff; font-size: 22px; font-weight: 500;">أولى خطوات النجاح التحضير اليومي</h2>
+            <h2 style="color:#ffd700; font-size: 65px; font-weight: 800;">بصمة تميز</h2>
+            <h2 style="color:#ffd700; font-size: 22px; font-weight: 500;">أولى خطوات النجاح التحضير اليومي</h2>
             <h2 style="margin:10px 0; font-size: 40px;">مدرسة</h2>
             <h2 style="margin:10px 0; font-size: 40px;">القطيف الثانوية</h2>
             <div style="font-size: 18px; margin-top: 20px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 20px;">
