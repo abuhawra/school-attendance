a61d05d59ccfae0f23d6ea5ed4c66c81669bda39
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-05 23:05:44 +0300

    Update header text in home page

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_wa_link(df, status_type, d):
 if st.session_state.page == "home":
     st.markdown('''
         <div class="main-header">
-            <h1 style="color:#ff9800; font-size: 65px; font-weight: 800; margin-bottom: 0;">بصمة تميز</h1>
+            <h1 style="color:#ff9800; font-size: 55px; font-weight: 800; margin-bottom: 0;">بَصمَة تَميُز</h1>
             <h2 style="color:#ff9800; font-size: 22px; font-weight: 500; margin-top: 0; line-height: 0.5;">أول خطوة للنجاح...التحضير</h2>
             <div style="margin-top: 20px; margin-bottom: 20px;">
                 <h2 class="school-name">مدرسة</h2>
