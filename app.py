4597db1a3eb8d877c8a715c8a6589e6f3a916c99
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-05 09:15:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ def get_wa_link(df, status_type, d):
 if st.session_state.page == "home":
     st.markdown('''
         <div class="main-header">
-            <h2 style="color:#ffd700; font-size: 65px; font-weight: 800;">بصمة تميز</h2>
-            <h2 style="color:#ffd700; font-size: 22px; font-weight: 500; margin-top: 0.5; line-height: 0.5;">أولى خطوات النجاح التحضير اليومي</h2>
-            <h2 style="margin:10px 0; font-size: 40px;">مدرسة</h2>
-            <h2 style="margin:10px 0; font-size: 40px;">القطيف الثانوية</h2>
+            <h2 style="color:#ffd700; font-size: 50px; font-weight: 800;">بصمة تميز</h2>
+            <h2 style="color:#ffd700; font-size: 20px; font-weight: 500; margin-top: 0.8; line-height: 0.8;">أولى خطوات النجاح التحضير اليومي</h2>
+            <h2 style="margin:10px 0; font-size: 35px;">مدرسة</h2>
+            <h2 style="margin:10px 0; font-size: 35px;">القطيف الثانوية</h2>
             <div style="font-size: 18px; margin-top: 20px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 20px;">
             </div>
             <h2 style="color:#ffd700; font-size: 24px; font-weight: 500;">مدير المدرسة</h2>
