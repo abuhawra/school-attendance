aaa5844e3a8fbffcb2e480fde61d2a7bd7510432
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-05 09:16:32 +0300

    Update header text for daily preparation message

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if st.session_state.page == "home":
     st.markdown('''
         <div class="main-header">
             <h2 style="color:#ffd700; font-size: 50px; font-weight: 800;">بصمة تميز</h2>
-            <h2 style="color:#ffd700; font-size: 20px; font-weight: 500; margin-top: 0.8; line-height: 0.8;">أولى خطوات النجاح التحضير اليومي</h2>
+            <h2 style="color:#ffd700; font-size: 20px; font-weight: 500; margin-top: 0.8; line-height: 0.8;">النجاح يبدأ بالتحضير اليومي</h2>
             <h2 style="margin:10px 0; font-size: 35px;">مدرسة</h2>
             <h2 style="margin:10px 0; font-size: 35px;">القطيف الثانوية</h2>
             <div style="font-size: 18px; margin-top: 20px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 20px;">
