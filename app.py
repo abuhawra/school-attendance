65890cecf8a8295c784c707a0380f1197baa12d2
abuhawra <108197842+abuhawra@users.noreply.github.com>
2026-05-05 23:07:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if st.session_state.page == "home":
                 <h2 class="school-name">مدرسة</h2>
                 <h2 class="school-name">القطيف الثانوية</h2>
             </div>
-            <div style="font-size: 18px; margin-top: 15px; border-top: 1px solid rgba(255,255,255,0.2); padding-top: 15px;">
+            <div style="font-size: 20px; margin-top: 15px; border-top: 2px solid rgba(255,255,255,0.2); padding-top: 15px;">
                 <p style="color:#ff9800; font-size: 24px; font-weight: 500; margin: 0;">مدير المدرسة</p>
                 <p style="color:#ffffff; font-size: 24px; font-weight: 500; margin: 0;">أ. فراس آل عبدالمحسن</p>
                 <p style="color:#ff9800; font-size: 22px; margin: 5px 0;">فكرة وبرمجة</p>
